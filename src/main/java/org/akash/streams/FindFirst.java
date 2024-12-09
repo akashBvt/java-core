d1740add0ab5d30ac64dd7f5c6604e132c40bba1
akash <akash0005007@gmail.com>
2024-12-09 21:37:21 +0530

    Update FindFirst.java

--- src/main/java/org/akash/streams/FindFirst.java
+++ src/main/java/org/akash/streams/FindFirst.java
@@ -1,14 +1,13 @@
 package org.akash.streams;
 
+import java.util.List;
+
 import org.akash.comparator.Comparator;
 import org.akash.model.Employee;
 
-import java.util.List;
-import java.util.Optional;
-
 public class FindFirst {
-    public static Optional<Employee> secondHighestSalaryEmployee(List<Employee> employees){
-        return employees.stream().sorted(Comparator.EmployeeSalaryComparator()).skip(1).findFirst();
+    public static Employee secondHighestSalaryEmployee(List<Employee> employees) throws Exception{
+        return employees.stream().sorted(Comparator.EmployeeSalaryComparator()).skip(1).findFirst().orElseThrow(()-> new Exception("Does not exsist"));
     }
     
 }
