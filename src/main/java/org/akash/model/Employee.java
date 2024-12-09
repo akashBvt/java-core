475346562aaf4c5259dc9485985e7f3376b2a242
akash <akash0005007@gmail.com>
2024-12-09 21:11:44 +0530

    Update Employee.java

--- src/main/java/org/akash/model/Employee.java
+++ src/main/java/org/akash/model/Employee.java
@@ -1,11 +1,20 @@
 package org.akash.model;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter@Setter@AllArgsConstructor
+@Getter@Setter
 public class Employee{
     private String Name;
     private double salary;
+    
+    public Employee(String name, double salary) {
+        Name = name;
+        this.salary = salary;
+        System.out.println("employe Object created");
+    }
+
+    static{
+        System.out.println("hello from static block of employee");
+    }
 
 }
